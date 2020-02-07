646d8008fd0429b6c948ebdf09e0803a79bb3194
haqpod <60613586+haqpod@users.noreply.github.com>
2020-02-07 11:26:51 +0100

    Update Player1Script.cs

--- Player1Script.cs
+++ Player1Script.cs
@@ -19,13 +19,23 @@ public class Player1Script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //input left right and up down
     {
        float horizontal = Input.GetAxis("Horizontal");
        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
 
         float vertical = Input.GetAxis("Vertical");
         transform.Translate(Vector2.up * vertical * Time.deltaTime * speed);
+        
+        //left and right sides will block the ball
+         if (transform.position.x < leftScreenEdge)
+        {
+            transform.position = new Vector2(leftScreenEdge, transform.position.y);
+        }
+        if (transform.position.x > rightScreenEdge)
+        {
+            transform.position = new Vector2(rightScreenEdge, transform.position.y);
+        }
 
        
       
